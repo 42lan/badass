21c6b7ff4f6c9a1764bb74fb66c9da61cfd4123f
42lan <amalsago@student.42.fr>
2022-05-05 12:19:19 +0200

    Move run.sh in root folder; build images in separate script

--- run.sh
+++ run.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-# Installation of Docker
+# Installation of Docker and GNS3
 # https://docs.docker.com/engine/install/ubuntu/
+# https://docs.gns3.com/docs/getting-started/installation/linux
 sudo apt-get update
+sudo add-apt-repository -y ppa:gns3/ppa
 sudo apt-get install -y ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo usermod -a -G docker $USER
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin gns3-gui gns3-server
 
-# Installation of GNS3
-# https://docs.gns3.com/docs/getting-started/installation/linux
-sudo add-apt-repository -y ppa:gns3/ppa
-sudo apt update                                
-sudo apt install -y gns3-gui gns3-server
-
-# Pull & Contruction of Docker images
-docker pull alpine:latest
-docker build --file=Dockerfile_frr --tag=router_sucho .
+# Add current user to the following groups
+sudo usermod -aG kvm,libvirt,docker,ubridge,wireshark $USER
