949a1989a2ceb1099085e7bc6e0e92968de19956
42lan <amalsago@student.42.fr>
2022-05-06 20:07:28 +0200

    Add script copying and applying the configuration

---
+++ p2/exec_confs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+running_containers=$(docker ps -q)
+if [[ ! -z $running_containers ]]
+then
+  for i in ${running_containers[@]}
+  do
+    container_info=$(echo "$i:$(docker exec $i hostname)")
+    hostname=${container_info#*:}
+    container_id=${container_info%:*}
+    case $hostname in
+      host_sucho-1)
+        docker cp host_sucho-1 $container_id:/
+        docker exec $container_id ash /host_sucho-1
+        ;;
+      host_sucho-2)
+        docker cp host_sucho-2 $container_id:/
+        docker exec $container_id ash /host_sucho-2
+        ;;
+      router_sucho-1)
+        docker cp router_sucho-1 $container_id:/
+        docker exec $container_id ash /router_sucho-1
+        ;;
+      router_sucho-2)
+        docker cp router_sucho-2 $container_id:/
+        docker exec $container_id ash /router_sucho-2
+        ;;
+    esac
+    echo "Configuration is applied on $container_id running the $hostname"
+  done
+  exit 0
+else
+  echo "No running containers"
+  exit 1
+fi
